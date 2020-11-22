86e06e9363b667f2f663dc0b27a4b803c65143ad
Mingxuan Xie <31252716+MingxuanXie@users.noreply.github.com>
2020-11-23 03:22:56 +0800

    add niu

--- js/buildmsn.js
+++ js/buildmsn.js
@@ -1,32 +1,22 @@
-var x = document.getElementById("voice"); 
-var z = document.getElementById("npc");
+// var x = document.getElementById("voice"); 
+// var z = document.getElementById("npc");
 var a = document.getElementById("startbutton");
-var b = document.getElementById("opponent");
-var c = document.getElementById("game");
-var d = document.getElementById("replay");
+// var d = document.getElementById("replay");
 var e = true;
+var n1 = document.getElementById("g1");
+var n2 = document.getElementById("g2");
+var n3 = document.getElementById("g3");
+var n4 = document.getElementById("g4");
+var n5 = document.getElementById("g5");
 document.getElementById("a-loader").addEventListener("loaded", activateStartButton);
 function activateStartButton() {
   a.style.display = "block";
 }
 function activateScene(foo) {    
-    z.components.sound.playSound();    
-    z.setAttribute('animation-mixer', {timeScale: 1});
+    n1.setAttribute('animation-mixer', {timeScale: 1});
+    n2.setAttribute('animation-mixer', {timeScale: 1});
+    n3.setAttribute('animation-mixer', {timeScale: 1});
+    n4.setAttribute('animation-mixer', {timeScale: 1});
+    n5.setAttribute('animation-mixer', {timeScale: 1});
     a.style.display = "none";
-    // setTimeout(function(){
-      b.object3D.visible = true;
-      c.style.display = "block";
-      z.setAttribute('sound', {src: '#loop-sound', loop: true});
-      z.components.sound.playSound();
-    // }, foo);
-}
-function activateGame() { 
-  b.components.gif.togglePlayback();
-  c.style.display = "none";
-  d.style.display = "block";
-}
-function activateReplay() { 
-  b.components.gif.togglePlayback();
-  c.style.display = "block";
-  d.style.display = "none";
 }
