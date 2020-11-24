058f595cfd4e3cfd3fd86302a9ab65cbd74311e1
Mingxuan Xie <31252716+MingxuanXie@users.noreply.github.com>
2020-11-24 15:13:01 +0800

    new model test

--- js/buildmsn.js
+++ js/buildmsn.js
@@ -1,13 +1,11 @@
-// var x = document.getElementById("voice"); 
-// var z = document.getElementById("npc");
 var a = document.getElementById("startbutton");
-// var d = document.getElementById("replay");
-var e = true;
 var n1 = document.getElementById("g1");
 var n2 = document.getElementById("g2");
 var n3 = document.getElementById("g3");
 var n4 = document.getElementById("g4");
 var n5 = document.getElementById("g5");
+var n6 = document.getElementById("g6");
+var n7 = document.getElementById("g7");
 document.getElementById("a-loader").addEventListener("loaded", activateStartButton);
 function activateStartButton() {
   a.style.display = "block";
@@ -18,5 +16,7 @@ function activateScene(foo) {
     n3.setAttribute('animation-mixer', {timeScale: 1});
     n4.setAttribute('animation-mixer', {timeScale: 1});
     n5.setAttribute('animation-mixer', {timeScale: 1});
+    n6.setAttribute('animation-mixer', {timeScale: 1});
+    n7.setAttribute('animation-mixer', {timeScale: 1});
     a.style.display = "none";
 }
